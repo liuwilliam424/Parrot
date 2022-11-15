ef2745dc1573791c565f62ea8b766e0bbbdb3d29
William Liu <liu.william424@gmail.com>
2022-11-15 12:25:58 -0500

    yoyoyo

--- firebase.js
+++ firebase.js
@@ -1,19 +1,53 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
-apiKey: "AIzaSyBUcuvOpLrA0L0tj1pE82YVwZIeBZcSfDI",
-authDomain: "parrot-cac27.firebaseapp.com",
-databaseURL: "https://parrot-cac27-default-rtdb.firebaseio.com",
-projectId: "parrot-cac27",
-storageBucket: "parrot-cac27.appspot.com",
-messagingSenderId: "40805265769",
-appId: "1:40805265769:web:53eeeede473a2680ce4df8",
-measurementId: "G-96J2CZKLYK"
+    apiKey: "AIzaSyBUcuvOpLrA0L0tj1pE82YVwZIeBZcSfDI",
+    authDomain: "parrot-cac27.firebaseapp.com",
+    databaseURL: "https://parrot-cac27-default-rtdb.firebaseio.com",
+    projectId: "parrot-cac27",
+    storageBucket: "parrot-cac27.appspot.com",
+    messagingSenderId: "40805265769",
+    appId: "1:40805265769:web:53eeeede473a2680ce4df8",
+    measurementId: "G-96J2CZKLYK"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+
+
+let sign_in_button = document.querySelector("#sign-in")
+sign_in_button.onclick = () => { alert("joe") }
+
+
+// sign_in_button.onclick = () => {signInWithPopup(auth, provider)}
 
