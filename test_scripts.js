155928057162a504984fe546bacece9944edf4a5
William Liu <liu.william424@gmail.com>
2022-11-15 15:36:44 -0500

    wow you can sign in with google

--- test_scripts.js
+++ test_scripts.js
@@ -22,11 +22,3 @@ function greeting(name) {
   }
   
   processUserInput(greeting);
-
-doSomething(function (result) {
-    doSomethingElse(result, function (newResult) {
-      doThirdThing(newResult, function (finalResult) {
-        console.log(`Got the final result: ${finalResult}`);
-      }, failureCallback);
-    }, failureCallback);
-  }, failureCallback);
