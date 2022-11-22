3b847e0a5f9aa6fc4fc67fed8f9186e513495d67
William Liu <liu.william424@gmail.com>
2022-11-21 22:07:16 -0500

    hey

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,44 @@
-// Import the functions you need from the SDKs you need
+//TODO: fix centering on sign out button
+
+
+//https://firebase.google.com/docs/web/learn-more#available-libraries
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
-import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBUcuvOpLrA0L0tj1pE82YVwZIeBZcSfDI",
-    authDomain: "parrot-cac27.firebaseapp.com",
-    databaseURL: "https://parrot-cac27-default-rtdb.firebaseio.com",
-    projectId: "parrot-cac27",
-    storageBucket: "parrot-cac27.appspot.com",
-    messagingSenderId: "40805265769",
-    appId: "1:40805265769:web:53eeeede473a2680ce4df8",
-    measurementId: "G-96J2CZKLYK"
+  apiKey: "AIzaSyBUcuvOpLrA0L0tj1pE82YVwZIeBZcSfDI",
+  authDomain: "parrot-cac27.firebaseapp.com",
+  databaseURL: "https://parrot-cac27-default-rtdb.firebaseio.com",
+  projectId: "parrot-cac27",
+  storageBucket: "parrot-cac27.appspot.com",
+  messagingSenderId: "40805265769",
+  appId: "1:40805265769:web:53eeeede473a2680ce4df8",
+  measurementId: "G-96J2CZKLYK"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-//Initialize Authenticaition
+//Initialize Authentication
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider();
-let uid = "NOT SIGNED IN YET"
 
+//Initialize HTML elements
 let sign_in_button = document.querySelector("#sign-in")
+let sign_out_button = document.querySelector("#sign-out")
+let welcome_message = document.querySelector("#welcome_message")
 
-
-
-sign_in_button.onclick = () => { 
-    signInWithPopup(auth, provider)
+sign_in_button.onclick = () => {
+  signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       // The signed-in user info.
       const user = result.user;
-      uid = user.uid;
       if (document.querySelector('#student').checked) {
         localStorage.setItem("Role", "Student")
         location.href = "./student_home.html"
@@ -45,7 +46,7 @@ sign_in_button.onclick = () => {
         localStorage.setItem("Role", "Teacher")
         location.href = "./teacher.html"
       } else {
-        // error bc they didnt check a btn b4 signing in, unless u have a default value set?
+        alert("Error: please select a role.")
       }
       // ...
     }).catch((error) => {
@@ -57,17 +58,35 @@ sign_in_button.onclick = () => {
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
-    }); }
+    });
+}
 
 onAuthStateChanged(auth, (user) => {
+  //checks the sign-in state of the user
   if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    console.log("Auth State Changed");
+    //code that runs if user is signed in
+    console.log("signed in")
     localStorage.setItem("UID", user.uid);
     localStorage.setItem("Name", user.displayName)
+    welcome_message.innerHTML = `Welcome back, ${localStorage.getItem("Name")}!`;
+    sign_in_button.innerHTML = "Let's get started!";
+    document.querySelectorAll('.radio-selection').forEach((element) => {
+      element.style.display = "none"
+    sign_out_button.style.display = "flex"
+    });
+
   } else {
-    // User is signed out
-    // ...
+    //code that runs if user is signed out
+    console.log("not signed in")
+    sign_out_button.style.display = "none"
   }
 });
+
+sign_out_button.onclick = () => {
+  signOut(auth).then(() => {
+    localStorage.clear()
+    location.reload()
+  }).catch((error) => {
+    alert("Error: failed to sign out")
+  });
+}
