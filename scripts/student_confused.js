a969b4db9566e8cee44dd7ca0acdadb8afcc7bca
DanielL248 <118243513+DanielL248@users.noreply.github.com>
2022-11-23 13:35:27 -0500

    text buttons
    textiewurbtks

--- scripts/student_confused.js
+++ scripts/student_confused.js
@@ -7,8 +7,15 @@ temp_b2.onclick = () => {location.href = "/html/student_session_main.html"}
 let temp_b3 = document.querySelector('#b3')
 temp_b3.onclick = () => {location.href = "/html/student_session_main.html"}
 
+let confused_text = document.querySelector('#text4')
+
 let temp_b4 = document.querySelector('#b4')
-temp_b4.onclick = () => {location.href = "/html/student_session_main.html"}
+temp_b4.onclick = () => {
+    console.log("Confused text: " + confused_text.value)
+    localStorage.setItem("Confused text: ", confused_text.value)
+    location.href = "/html/student_session_main.html"
+
+}
 
 let temp_b5 = document.querySelector('#b5')
 temp_b5.onclick = () => {location.href = "/html/student_session_main.html"}
