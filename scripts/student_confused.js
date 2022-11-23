3194da97fc8aa40a8d53e7f59bcd407a25de89d0
DanielL248 <118243513+DanielL248@users.noreply.github.com>
2022-11-23 12:48:09 -0500

    confused pagea
    PHEPLEHPLPL

---
+++ scripts/student_confused.js
@@ -0,0 +1,14 @@
+let temp_b1 = document.querySelector('#b1')
+temp_b1.onclick = () => {location.href = "/html/student_session_main.html"}
+
+let temp_b2 = document.querySelector('#b2')
+temp_b2.onclick = () => {location.href = "/html/student_session_main.html"}
+
+let temp_b3 = document.querySelector('#b3')
+temp_b3.onclick = () => {location.href = "/html/student_session_main.html"}
+
+let temp_b4 = document.querySelector('#b4')
+temp_b4.onclick = () => {location.href = "/html/student_session_main.html"}
+
+let temp_b5 = document.querySelector('#b5')
+temp_b5.onclick = () => {location.href = "/html/student_session_main.html"}
