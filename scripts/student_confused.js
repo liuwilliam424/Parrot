6e022ba4c0fa1c2e18a149cd745f6b7ddf0d5c12
DanielL248 <118243513+DanielL248@users.noreply.github.com>
2022-11-23 13:39:21 -0500

    hnref change
    134

--- scripts/student_confused.js
+++ scripts/student_confused.js
@@ -1,11 +1,11 @@
 let temp_b1 = document.querySelector('#b1')
-temp_b1.onclick = () => {location.href = "/html/student_session_main.html"}
+temp_b1.onclick = () => {location.href = "../html/student_session_main.html"}
 
 let temp_b2 = document.querySelector('#b2')
-temp_b2.onclick = () => {location.href = "/html/student_session_main.html"}
+temp_b2.onclick = () => {location.href = "../html/student_session_main.html"}
 
 let temp_b3 = document.querySelector('#b3')
-temp_b3.onclick = () => {location.href = "/html/student_session_main.html"}
+temp_b3.onclick = () => {location.href = "../html/student_session_main.html"}
 
 let confused_text = document.querySelector('#text4')
 
@@ -13,9 +13,9 @@ let temp_b4 = document.querySelector('#b4')
 temp_b4.onclick = () => {
     console.log("Confused text: " + confused_text.value)
     localStorage.setItem("Confused text: ", confused_text.value)
-    location.href = "/html/student_session_main.html"
+    location.href = "../html/student_session_main.html"
 
 }
 
 let temp_b5 = document.querySelector('#b5')
-temp_b5.onclick = () => {location.href = "/html/student_session_main.html"}
+temp_b5.onclick = () => {location.href = "../html/student_session_main.html"}
